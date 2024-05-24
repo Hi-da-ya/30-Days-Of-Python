b90613475d4d09e0328f1f26eac603752a055451
Hidaya Wetsakhulia <hidayawetsa@gmail.com>
2024-05-24 16:20:49 +0300

    day 1 completed

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -1,5 +1,6 @@
 # Introduction
 # Day 1 - 30DaysOfPython Challenge
+import math
 
 print(2 + 3)   # addition(+)
 print(3 - 1)   # subtraction(-)
@@ -9,13 +10,33 @@ print(3 ** 2)  # exponential(**)
 print(3 % 2)   # modulus(%)
 print(3 // 2)  # Floor division operator(//)
 
-# Checking data types
 
-print(type(10))                  # Int
-print(type(3.14))                # Float
-print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
-print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Set
-print(type((9.8, 3.14, 2.7)))    # Tuple
+#Data types in python
+#1. String
+name = "Elsa"
+#2. Number- integers and float
+age = 10
+height = 5.8
+#3. List
+friends = ["Anna", "Olaf", "Spot", "Christoph"]
+#4. Tuples
+kingdoms = ("Enchancia", "Auradon")
+#5. Sets
+vk = {"Mal", "Evie", "Carlos"}
+#6. Dicts
+person ={
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'country':'Finland', 
+    'age':250, 
+    'is_married':True,
+    'skills':['JS', 'React', 'Node', 'Python']
+}
+
+#Euclidian distance is distance between two points
+a = (2,3)
+b = (10, 8)
+
+print(math.dist(a, b))
+
+
